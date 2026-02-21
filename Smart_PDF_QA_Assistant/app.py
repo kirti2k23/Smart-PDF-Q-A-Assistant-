b604c2882fdb09f90428e0fa29f8cb11a534242d
kirti2k23 <kv.edu14@gmail.com>
2026-02-21 16:47:26 +0530

    'Fourth Commit"

--- Smart_PDF_QA_Assistant/app.py
+++ Smart_PDF_QA_Assistant/app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from rag_pipeline import db, embeddings
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_llms import Ollama
+from langchain_ollama import OllamaLLM
 import streamlit as st
 import os
 
@@ -10,15 +10,20 @@ load_dotenv()  # Load environment variables from .env file
 #Langsmith API key and tracing configuration
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 os.environ["LANGSMITH_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "MY-FIRST-RAG-PROJECT"
 
 # Create a Prompt Template for the question-answering task
 prompt_template = ChatPromptTemplate.from_messages([
-    ("system", "You are helpful AI."),
-    ("human", "{question}")
+    ("system", 
+     "You are an AI assistant. Answer ONLY from the provided context. "
+     "If answer is not in context, say 'I don't know'."),
+     
+    ("human", 
+     "Context:\n{context}\n\nQuestion:\n{question}")
 ])
 
 # Initialize the Ollama LLM
-llm = Ollama(model = 'llama3')
+llm = OllamaLLM(model = 'llama3')
 chain = prompt_template | llm
 
 # Streamlit app
@@ -30,10 +35,22 @@ question = st.text_input("Enter your question here:")
 
 if question:
     # Retrieve relevant documents from the FAISS vector store
-    relevant_docs = db.similarity_search(question, k=3)
+    results = db.similarity_search_with_score(question, k=10)
+    relevant_docs = [doc for doc, score in results if score < 0.5]
+
+    # Convert documents to text
+    context = "\n\n".join([doc.page_content for doc in relevant_docs])
     
     # Generate an answer using the retrieved documents and the LLM
-    answer = chain.run(question=question, context=relevant_docs)
+    answer = chain.invoke({
+    "question": question,
+    "context": context
+})
     
     st.write("Answer:")
+    # Display the retrieved documents and the generated answer
+    for doc,score in results:
+        st.write("Score:", score)
+        st.write(doc.page_content[:300])
+        st.write("---")
     st.write(answer)
--- Smart_PDF_QA_Assistant/rag_pipeline.py
+++ Smart_PDF_QA_Assistant/rag_pipeline.py
@@ -11,6 +11,7 @@ load_dotenv()  # Load environment variables from .env file
 #Langsmith API key and tracing configuration
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 os.environ["LANGSMITH_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "MY-FIRST-RAG-PROJECT"
 
 # Get pdf file directory
 pdf_dir = os.path.abspath("data/AI_Engineer_Roadmap_2026.pdf")   
@@ -20,13 +21,13 @@ pdf = PyPDFLoader("data/AI_Engineer_Roadmap_2026.pdf")
 docs = pdf.load()
 
 # Split the text present in the PDF into smaller chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200,add_start_index=True,)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 100,add_start_index=True,)
 
 documents = text_splitter.split_documents(docs)
 print(f"Split blog post into {len(documents)} sub-documents.")
 
 # Create embedding for the text chunks using Ollama embeddings
-embeddings = OllamaEmbeddings(model = 'llama3')
+embeddings = OllamaEmbeddings(model = 'nomic-embed-text')
 
 # Create a FAISS vector store from the documents and their embeddings
 db = FAISS.from_documents(documents, embeddings)
